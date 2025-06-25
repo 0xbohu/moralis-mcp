1756fd32b7e6355e1e8dfb19407595c5de106d0a
Bo Hu <0xbohu@gmail.com>
2025-06-25 18:48:18 +1000

    init

--- src/types/open-api-document-x.ts
+++
@@ -1,9 +0,0 @@
-import type { OpenAPIV3 } from 'openapi-types';
-
-interface DocumentX {
-  'x-mcp-prompt'?: string;
-}
-
-export interface OpenAPIV3DocumentX extends OpenAPIV3.Document<DocumentX> {
-  'x-mcp-blacklist': string[];
-}
